3f59b961d11371c1a9d0a22b78b20ba1453f5d14
leonfyr <leonsz2009@qq.com>
2025-11-03 16:44:12 +0800

    title

--- src/config.ts
+++ src/config.ts
@@ -9,8 +9,8 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "ctww",
-	subtitle: "blog",
+	title: "涡の小屋",
+	subtitle: "ctww",
 	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 220, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
