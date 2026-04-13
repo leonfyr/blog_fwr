c1f83439bfea859f64598f5e889c946caba3f441
leonfyr <leonsz2009@qq.com>
2026-04-14 00:07:26 +0800

    喵喵！

--- src/config.ts
+++ src/config.ts
@@ -19,7 +19,7 @@ export const siteConfig: SiteConfig = {
 	banner: {
 		enable: true,
 		src: "assets/images/banner2.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-		position: "bottom", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
+		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
 			enable: true, // Display the credit text of the banner image
 			text: "晴いちばん", // Credit text to be displayed
